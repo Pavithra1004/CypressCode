a83dce57db6faa822a7efa229cecc13ac41be8c1
Pavithra1004 <pavithra.rpee12@gmail.com>
2022-09-29 15:33:44 +0530

    second commit

--- cypress/e2e/Test3.cy.js
+++ cypress/e2e/Test3.cy.js
@@ -1,5 +1,6 @@
 it("Google",function()
 {
+    //Google search
     cy.visit('https://www.google.com/',{timeout: 4000});
     cy.xpath("//input[@class='gLFyf gsfi']").type("data");
     cy.xpath("//input[@value='Google Search'  and @aria-label='Google Search' ]/ancestor::div[@class='CqAVzb lJ9FBc']").click();
